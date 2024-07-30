2992865bcbdeabc76d2bec9e7e860aa208a69b77
David Paulsson <david@davidpaulsson.se>
2024-07-30 15:18:25 +0200

    Add teknikomraden slider

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -9,6 +9,7 @@ import IconAffarshantering from './icon-affarshantering.png';
 import IconAiPlattform from './icon-ai-plattform.png';
 import IconCrmIntegrering from './icon-crm-integrering.png';
 import IconDokumenthantering from './icon-dokumenthantering.png';
+import { TeknikOmraden } from './teknikomraden';
 
 export default function Home() {
   return (
@@ -24,57 +25,60 @@ export default function Home() {
         <Header theme="light" />
         <Hero />
       </div>
+      <main>
+        <div className="container py-6">
+          <FeatureSection
+            align="left"
+            topIcon={<Image src={IconAiPlattform} alt="" width={40} height={40} />}
+            topTitle="AI-plattform"
+            title="Supersäkert och enkelt med Ormeo®"
+            content="Ta total kontroll över företagsinformation med vår AI-drivna plattform Ormeo®. Vår avancerade AI tolkar varje detalj i årsredovisningar, finansiell data, myndighetsinformation, offentlig information, sociala företagsprofiler, adressdata och mycket mer. Upptäck kraften i datadriven insikt och gör smartare affärsbeslut på ett ögonblick."
+            linkHref="/ormeo"
+            linkLabel="Testa Ormeo"
+          />
 
-      <main className="container py-6">
-        <FeatureSection
-          align="left"
-          topIcon={<Image src={IconAiPlattform} alt="" width={40} height={40} />}
-          topTitle="AI-plattform"
-          title="Supersäkert och enkelt med Ormeo®"
-          content="Ta total kontroll över företagsinformation med vår AI-drivna plattform Ormeo®. Vår avancerade AI tolkar varje detalj i årsredovisningar, finansiell data, myndighetsinformation, offentlig information, sociala företagsprofiler, adressdata och mycket mer. Upptäck kraften i datadriven insikt och gör smartare affärsbeslut på ett ögonblick."
-          linkHref="/ormeo"
-          linkLabel="Testa Ormeo"
-        />
+          <FeatureSection
+            align="right"
+            topIcon={<Image src={IconAffarshantering} alt="" width={40} height={40} />}
+            topTitle="Affärshantering"
+            title="En smartare och säkrare affärshantering med vår AI-plattform"
+            content="TIC:s AI-plattform övervakar ständigt tusentals datakällor för att identifiera avvikelser och mönster i register, årsredovisningar och andra informationskällor. Med Ormeo® eliminerar du obehagliga överraskningar och får full koll på vem du gör affärer med. Säker och smart affärshantering har aldrig varit enklare."
+            linkHref="/ormeo"
+            linkLabel="Läs mer om vår AI-teknik"
+          />
 
-        <FeatureSection
-          align="right"
-          topIcon={<Image src={IconAffarshantering} alt="" width={40} height={40} />}
-          topTitle="Affärshantering"
-          title="En smartare och säkrare affärshantering med vår AI-plattform"
-          content="TIC:s AI-plattform övervakar ständigt tusentals datakällor för att identifiera avvikelser och mönster i register, årsredovisningar och andra informationskällor. Med Ormeo® eliminerar du obehagliga överraskningar och får full koll på vem du gör affärer med. Säker och smart affärshantering har aldrig varit enklare."
-          linkHref="/ormeo"
-          linkLabel="Läs mer om vår AI-teknik"
-        />
+          <FeatureSection
+            align="left"
+            topIcon={<Image src={IconCrmIntegrering} alt="" width={40} height={40} />}
+            topTitle="CRM-integrering"
+            title="Öka din försäljning och dina intäkter direkt"
+            content="Med vår AI-plattform Ormeo® kan du enkelt identifiera nya potentiella kunder genom prospektering och smarta urval. Använd vårt webbverktyg för personlig och effektiv marknadsföring, och integrera Ormeo® helt sömlöst med ditt befintliga CRM-system. Upptäck hur det känns att på riktigt revolutionera sin försäljning."
+            linkHref="/ormeo"
+            linkLabel="Se integrationen live"
+          />
 
-        <FeatureSection
-          align="left"
-          topIcon={<Image src={IconCrmIntegrering} alt="" width={40} height={40} />}
-          topTitle="CRM-integrering"
-          title="Öka din försäljning och dina intäkter direkt"
-          content="Med vår AI-plattform Ormeo® kan du enkelt identifiera nya potentiella kunder genom prospektering och smarta urval. Använd vårt webbverktyg för personlig och effektiv marknadsföring, och integrera Ormeo® helt sömlöst med ditt befintliga CRM-system. Upptäck hur det känns att på riktigt revolutionera sin försäljning."
-          linkHref="/ormeo"
-          linkLabel="Se integrationen live"
-        />
+          <FeatureSection
+            align="right"
+            topIcon={<Image src={IconDokumenthantering} alt="" width={40} height={40} />}
+            topTitle="Dokumenthantering"
+            title="Dataanalys och skräddarsydd rådgivning"
+            content="Kombinera dina egna data med hundratals datakällor i vår AI-plattform Ormeo för att få helt nya insikter om dina affärsrelationer. Ormeo® hanterar alla typer av dokument- och filformat samt bilder, och tolkar både strukturerad och ostrukturerad data. Upptäck kraften i datadriven rådgivning och förvandla din verksamhet."
+            linkHref="/ormeo"
+            linkLabel="Testa Ormeo"
+          />
 
-        <FeatureSection
-          align="right"
-          topIcon={<Image src={IconDokumenthantering} alt="" width={40} height={40} />}
-          topTitle="Dokumenthantering"
-          title="Dataanalys och skräddarsydd rådgivning"
-          content="Kombinera dina egna data med hundratals datakällor i vår AI-plattform Ormeo för att få helt nya insikter om dina affärsrelationer. Ormeo® hanterar alla typer av dokument- och filformat samt bilder, och tolkar både strukturerad och ostrukturerad data. Upptäck kraften i datadriven rådgivning och förvandla din verksamhet."
-          linkHref="/ormeo"
-          linkLabel="Testa Ormeo"
-        />
+          <FeatureSection
+            align="left"
+            topIcon={<Image src={IconCrmIntegrering} alt="" width={40} height={40} />}
+            topTitle="Mer än kreditupplysingar"
+            title="Mer än ett vanligt kreditupplysningsbolag"
+            content="Vi är mer än ett vanligt kreditupplysningsbolag – vi gör affärer säkrare och tryggare genom att ge dig bättre koll på vem du gör affärer med. Vår avancerade teknik bearbetar årsredovisningar, registerinformation och andra källor för att identifiera avvikelser. Våra tjänster är helt kostnadsfria för offentliga aktörer och icke vinstdrivna organisationer."
+            linkHref="/ormeo"
+            linkLabel="Börja kostnadsfritt"
+          />
+        </div>
 
-        <FeatureSection
-          align="left"
-          topIcon={<Image src={IconCrmIntegrering} alt="" width={40} height={40} />}
-          topTitle="Mer än kreditupplysingar"
-          title="Mer än ett vanligt kreditupplysningsbolag"
-          content="Vi är mer än ett vanligt kreditupplysningsbolag – vi gör affärer säkrare och tryggare genom att ge dig bättre koll på vem du gör affärer med. Vår avancerade teknik bearbetar årsredovisningar, registerinformation och andra källor för att identifiera avvikelser. Våra tjänster är helt kostnadsfria för offentliga aktörer och icke vinstdrivna organisationer."
-          linkHref="/ormeo"
-          linkLabel="Börja kostnadsfritt"
-        />
+        <TeknikOmraden />
       </main>
     </>
   );
---
+++ src/app/(homepage)/teknikomraden.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
+
+export const TeknikOmraden = () => {
+  return (
+    <div className="bg-gradient-to-b from-[#CDD4F6] to-[#F3F2FF] py-32">
+      <div className="container">
+        <h2 className="mb-16 text-pretty text-2xl sm:text-3xl md:max-w-[20ch] md:text-4xl lg:text-5xl">
+          Så här driver vi våra tre teknikområden
+        </h2>
+      </div>
+
+      <Carousel
+        opts={{
+          containScroll: false,
+          align: (viewSize) => {
+            const containerMaxWidth = 1280;
+            const paddingInPixels = 32;
+
+            if (viewSize > containerMaxWidth) {
+              const offset = (viewSize - containerMaxWidth) / 2 + paddingInPixels;
+              return offset;
+            }
+
+            return paddingInPixels;
+          },
+        }}
+      >
+        <CarouselContent className="gap-6">
+          <CarouselItem className="max-w-[calc(100vw-3rem)] rounded-3xl bg-white p-6 md:max-w-[520px] md:p-8 lg:p-10">
+            <h3 className="mb-8 text-pretty text-xl sm:text-2xl md:max-w-[20ch] md:text-3xl lg:text-4xl">
+              Snabb och smart dokumentbearbetning
+            </h3>
+            <p>
+              AI-plattformens dokumentbearbetning (IDP) läser, extraherar, kategoriserar och strukturerar data från dokument, t.ex
+              årsredovisningar i både digitalt och icke-digitalt format.
+            </p>
+          </CarouselItem>
+          <CarouselItem className="max-w-[calc(100vw-3rem)] rounded-3xl bg-white p-10 md:max-w-[520px]">
+            <h3 className="mb-8 text-pretty text-xl sm:text-2xl md:max-w-[20ch] md:text-3xl lg:text-4xl">Avvikelsedetektion</h3>
+            <p>
+              AI-plattformen arbetar med stora datamängder och urskiljer data som är ovanliga, annorlunda eller på annat sätt sticker ut
+              från mängden. Våra experter bygger metoder och modellering för säkra upplysningar om företagen.
+            </p>
+          </CarouselItem>
+          <CarouselItem className="max-w-[calc(100vw-3rem)] rounded-3xl bg-white p-10 md:max-w-[520px]">
+            <h3 className="mb-8 text-pretty text-xl sm:text-2xl md:max-w-[20ch] md:text-3xl lg:text-4xl">Lorem ipsum</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti cupiditate est nihil tempora recusandae! Praesentium, ab
+              eaque! Quaerat qui odio doloremque similique adipisci quidem consequuntur mollitia obcaecati. Accusamus, dolorem mollitia!
+            </p>
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
