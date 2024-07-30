d9ab6001c64dab232b77e82f6e6c001253592cb4
David Paulsson <david@davidpaulsson.se>
2024-07-30 15:28:30 +0200

    Update teknikomraden.tsx

--- src/app/(homepage)/teknikomraden.tsx
+++ src/app/(homepage)/teknikomraden.tsx
@@ -52,8 +52,6 @@ export const TeknikOmraden = () => {
             </p>
           </CarouselItem>
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
       </Carousel>
     </div>
   );
