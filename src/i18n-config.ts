d3a356a4c567baa8beb2da20ff903a0903edb4f7
David Paulsson <david@davidpaulsson.se>
2024-07-31 14:40:07 +0200

    Add more languages because why not

--- src/i18n-config.ts
+++ src/i18n-config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
   defaultLocale: 'sv',
-  locales: ['sv', 'en'],
+  locales: ['sv', 'en', 'no', 'de'],
 } as const;
 
 export type Locale = (typeof i18n)['locales'][number];
--- src/lib/get-dictionary.ts
+++ src/lib/get-dictionary.ts
@@ -3,6 +3,8 @@ import 'server-only';
 const dictionaries = {
   en: () => import('../dictionaries/en.json').then((module) => module.default),
   sv: () => import('../dictionaries/sv.json').then((module) => module.default),
+  de: () => import('../dictionaries/de.json').then((module) => module.default),
+  no: () => import('../dictionaries/no.json').then((module) => module.default),
 };
 
 export const getDictionary = async (locale: string) => {
@@ -11,6 +13,10 @@ export const getDictionary = async (locale: string) => {
       return await dictionaries.en();
     case 'sv':
       return await dictionaries.sv();
+    case 'de':
+      return await dictionaries.de();
+    case 'no':
+      return await dictionaries.no();
     default:
       throw new Error(`Unsupported locale: ${locale}`);
   }
