f9722f105e8293b3c149a9f695f9a3e3c3175468
David Paulsson <david@davidpaulsson.se>
2024-07-31 14:45:40 +0200

    Fix: Type error

--- src/app/[lang]/pages/layout.tsx
+++ src/app/[lang]/pages/layout.tsx
@@ -2,13 +2,15 @@ import { Header } from '@/components/header';
 
 export default function PagesLayout({
   children,
+  params,
 }: Readonly<{
   children: React.ReactNode;
+  params: { lang: string };
 }>) {
   return (
     <>
       <div className="border-b py-6">
-        <Header theme="dark" />
+        <Header theme="dark" lang={params.lang} />
       </div>
       <main className="container py-12">{children}</main>
     </>
