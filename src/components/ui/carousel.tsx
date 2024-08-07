dd42c4f9575ce3d3094b2b140963b9095b677e11
David Paulsson <david@davidpaulsson.se>
2024-08-07 10:26:49 +0200

    Add slight shadow to carousel items

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -141,7 +141,7 @@ const CarouselContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HT
   const { carouselRef, orientation } = useCarousel();
 
   return (
-    <div ref={carouselRef} className="overflow-hidden">
+    <div ref={carouselRef} className="overflow-hidden pb-1">
       <div ref={ref} className={cn('flex', orientation === 'horizontal' ? '-ml-4' : '-mt-4 flex-col', className)} {...props} />
     </div>
   );
@@ -150,7 +150,13 @@ CarouselContent.displayName = 'CarouselContent';
 
 const CarouselItem = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => {
   return (
-    <div ref={ref} role="group" aria-roledescription="slide" className={cn('min-w-0 shrink-0 grow-0 basis-full', className)} {...props} />
+    <div
+      ref={ref}
+      role="group"
+      aria-roledescription="slide"
+      className={cn('min-w-0 shrink-0 grow-0 basis-full shadow', className)}
+      {...props}
+    />
   );
 });
 CarouselItem.displayName = 'CarouselItem';
