9042fbc36d6037c2e10d651368fe35f512ade2e9
David Paulsson <david@davidpaulsson.se>
2024-08-28 16:12:53 +0200

    Fix: Metadata for Next 14

--- src/app/[region]/page.tsx
+++ src/app/[region]/page.tsx
@@ -9,31 +9,39 @@ export function generateStaticParams() {
 
 export default async function Page({ params }: Readonly<{ params: { slug: string[] } }>) {
   return (
-    <>
-      <title>The Intelligence Company</title>
-      <meta name="description" content="The Intelligence Company AB (publ) 559487-1682" />
-      <meta property="og:image" content="/og-image.png" />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
-      <meta property="og:image:alt" content="The Intelligence Company" />
-
-      <main className="p-8 text-base text-slate-950 md:p-16">
-        <Logo className="mb-16 h-auto w-16" />
-        <h1 className="mb-2 text-2xl">The Intelligence Company AB (publ) 559487-1682</h1>
-        <address className="max-w-prose text-base not-italic text-slate-600">
-          Press, questions, or general inquiries please contact
-          <br />
-          <a href="https://www.linkedin.com/in/nylanderjens/">Jens Nylander</a>, CEO
-          <br />
-          <a href="tel:0046733670882" className="text-tic-blue transition-colors hover:text-tic-blue-light">
-            +46 73-367 08 82
-          </a>
-          <br />
-          <a href="mailto:info@tic.io" className="text-tic-blue transition-colors hover:text-tic-blue-light">
-            info@tic.io
-          </a>
-        </address>
-      </main>
-    </>
+    <main className="p-8 text-base text-slate-950 md:p-16">
+      <Logo className="mb-16 h-auto w-16" />
+      <h1 className="mb-2 text-2xl">The Intelligence Company AB (publ) 559487-1682</h1>
+      <address className="max-w-prose text-base not-italic text-slate-600">
+        Press, questions, or general inquiries please contact
+        <br />
+        <a href="https://www.linkedin.com/in/nylanderjens/">Jens Nylander</a>, CEO
+        <br />
+        <a href="tel:0046733670882" className="text-tic-blue transition-colors hover:text-tic-blue-light">
+          +46 73-367 08 82
+        </a>
+        <br />
+        <a href="mailto:info@tic.io" className="text-tic-blue transition-colors hover:text-tic-blue-light">
+          info@tic.io
+        </a>
+      </address>
+    </main>
   );
 }
+
+const metadata = {
+  title: 'The Intelligence Company',
+  description: 'The Intelligence Company AB (publ) 559487-1682',
+  openGraph: {
+    images: [
+      {
+        url: 'https://www.tic.io/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'The Intelligence Company',
+      },
+    ],
+  },
+};
+
+export { metadata };
--- src/app/[region]/statistik/arsredovisningar/page.tsx
+++ src/app/[region]/statistik/arsredovisningar/page.tsx
@@ -1,10 +1,22 @@
 import { Reports } from './_components/reports';
 
 export default async function Page() {
-  return (
-    <>
-      <title>The Intelligence Company</title>
-      <Reports />
-    </>
-  );
+  return <Reports />;
 }
+
+const metadata = {
+  title: 'Finansiella rapporter (års- och delårsrapporter) | The Intelligence Company',
+  description: 'Statistik om registrerade finansiella rapporter (års- och delårsrapporter) hos Bolagsverket. Uppdateras varje dag.',
+  openGraph: {
+    images: [
+      {
+        url: 'https://www.tic.io/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'The Intelligence Company - Finansiella rapporter (års- och delårsrapporter)',
+      },
+    ],
+  },
+};
+
+export { metadata };
