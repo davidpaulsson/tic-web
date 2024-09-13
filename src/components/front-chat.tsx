4e7b103973375532e29ac23787089a378c3e772c
David Paulsson <david@davidpaulsson.se>
2024-09-13 11:41:09 +0200

    chore: Add lazy loading for front chat script

--- src/components/front-chat.tsx
+++ src/components/front-chat.tsx
@@ -1,18 +1,22 @@
-// @/components/front-chat.tsx
 'use client';
 
 import Script from 'next/script';
 
-// @/components/front-chat.tsx
-
-export function FrontChat() {
-  return <Script id="front-chat-script" src="https://chat-assets.frontapp.com/v1/chat.bundle.js" onLoad={initFrontChat}></Script>;
+declare global {
+  interface Window {
+    FrontChat: (command: string, options: { chatId: string; useDefaultLauncher: boolean }) => void;
+  }
 }
 
-function initFrontChat() {
-  //@ts-expect-error
-  window.FrontChat('init', {
-    chatId: '1ece8deb30d271d198fe682542a170a3',
-    useDefaultLauncher: true,
-  });
-}
+export const FrontChat = () => (
+  <Script
+    src="https://chat-assets.frontapp.com/v1/chat.bundle.js"
+    strategy="lazyOnload"
+    onLoad={() => {
+      window.FrontChat('init', {
+        chatId: '1ece8deb30d271d198fe682542a170a3',
+        useDefaultLauncher: true,
+      });
+    }}
+  />
+);
