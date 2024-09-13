cb180012bcb018d963bab46cf7fd548a2601228c
David Paulsson <david@davidpaulsson.se>
2024-09-13 14:56:01 +0200

    Update product-feature.tsx

--- src/components/product-feature.tsx
+++ src/components/product-feature.tsx
@@ -8,10 +8,10 @@ import { Button } from '@/components/ui/button';
 
 export const ProductFeature = (props: ContentfulBlockProductFeature['fields'] & { align: 'left' | 'right' }) => (
   <section className="container">
-    <div className="grid-cols-5 rounded-lg border border-tic-stroke bg-tic-fill md:grid">
-      <div className="col-span-2 hidden md:block" />
+    <div className="grid-cols-12 overflow-hidden rounded-lg border border-tic-stroke bg-tic-fill md:grid">
+      <div className="col-span-7 hidden bg-tic-purple-light md:block" />
       <div
-        className={cn('col-span-3 p-16', {
+        className={cn('col-span-5 p-16', {
           'order-[-1]': props.align === 'left',
         })}
       >
@@ -19,13 +19,11 @@ export const ProductFeature = (props: ContentfulBlockProductFeature['fields'] & 
 
         <div>
           <h2 className="mb-2 text-balance text-2xl md:text-3xl">{props.title}</h2>
-          <p className={cn('text-pretty text-lg text-tic-light md:max-w-[50ch]', { 'mb-10': !!props.cta })}>{props.description}</p>
+          <p className={cn('text-pretty text-lg text-tic-light', { 'mb-10': !!props.cta })}>{props.description}</p>
 
           {props.cta && (
             <Button asChild>
-              <Link href={props.cta.fields.link.fields.slug} className="group flex gap-2">
-                {props.cta.fields.label} <ChevronRight className="h-5 w-5 text-[#C8B8DC] transition-transform group-hover:translate-x-1" />
-              </Link>
+              <Link href={props.cta.fields.link.fields.slug}>{props.cta.fields.label}</Link>
             </Button>
           )}
         </div>
